4862f3b0bd1c97c719af06a958fade854ae6f3b4
Pushpendra7409 <pushpendra7409@gmail.com>
2024-10-11 09:19:25 +0530

    Initialize files and folder structure

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div>
+      <h1>Hello</h1>
+    </div>
+  </React.StrictMode>
+);
+
